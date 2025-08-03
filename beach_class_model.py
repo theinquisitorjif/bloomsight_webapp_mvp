26b7673abef921ae438606b0f0915e8a4690e63a
Ja'Rell I. Felix <imonfelix.j@gmail.com>
2025-08-03 16:20:39 -0400

    Update beach_class_model.py

--- beach_class_model.py
+++ beach_class_model.py
@@ -6,6 +6,7 @@ import requests_cache
 from retry_requests import retry
 from datetime import datetime
 
+#add lighstning data later
 
 #Pull in data from Open-Meteo API
 # Setup the Open-Meteo API client with cache and retry on error
@@ -13,7 +14,6 @@ cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
 retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
 openmeteo = openmeteo_requests.Client(session=retry_session)
 # Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
 	"latitude": 29.651634,
