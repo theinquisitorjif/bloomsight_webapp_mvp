73c9c5a3d949b6230302f0ef40cc93075f47c735
sophiadadla <dadlas@ufl.edu>
2025-08-05 20:39:23 -0400

    added map and navbar

--- frontend/src/pages/home-page.tsx
+++ frontend/src/pages/home-page.tsx
@@ -1,5 +1,31 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "../components/ui/navigation-menu";
+import Map from '../components/ui/map';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <Map />
+    </div>
+  );
 };
 
 export default HomePage;
