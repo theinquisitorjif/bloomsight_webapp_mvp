a8cc86fc392552107715c0198bdc256ffbcd0ae7
jaedonspurlock01 <jaedonaspurlock@outlook.com>
2025-08-13 16:27:42 -0400

    refactor: Updated photos

--- frontend/src/components/beach/beach-header.tsx
+++ frontend/src/components/beach/beach-header.tsx
@@ -1,18 +1,30 @@
-import { Bookmark, Car, MapPin, Share, Star } from "lucide-react";
+import { Car, MapPin, Star } from "lucide-react";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 import { BeachPhotos } from "./beach-photos";
 
-export const BeachHeader = () => {
+interface BeachHeaderProps {
+  name: string;
+  location: string;
+  lat: number;
+  lng: number;
+}
+
+export const BeachHeader = ({
+  name = "No Name",
+  location = "Unknown",
+  lat,
+  lng,
+}: BeachHeaderProps) => {
   return (
     <section id="beach-header">
-      <BeachPhotos />
+      <BeachPhotos name={name} />
 
-      <div className="flex justify-between">
+      <div className="flex justify-between mt-4">
         <div className="space-y-2">
           <h1 className="text-2xl lg:text-3xl xl:text-4xl font-semibold tracking-tight">
-            Carlsbad Beach
+            {name}
           </h1>
           <div className="flex-col sm:flex-row flex sm:items-center gap-4">
             <div className="flex items-center gap-2">
@@ -25,13 +37,13 @@ export const BeachHeader = () => {
 
             <div className="flex items-center gap-2">
               <MapPin size={18} />
-              <p>Carlsbad, CA</p>
+              <p>{location}</p>
             </div>
           </div>
         </div>
 
         <div className="flex items-start sm:items-center gap-4">
-          <Tooltip>
+          {/* <Tooltip>
             <TooltipTrigger>
               <Button
                 variant="outline"
@@ -41,8 +53,8 @@ export const BeachHeader = () => {
               </Button>
             </TooltipTrigger>
             <TooltipContent>Bookmark Beach</TooltipContent>
-          </Tooltip>
-          <Tooltip>
+          </Tooltip> */}
+          {/* <Tooltip>
             <TooltipTrigger>
               <Button
                 variant="outline"
@@ -52,17 +64,28 @@ export const BeachHeader = () => {
               </Button>
               <TooltipContent>Share Beach</TooltipContent>
             </TooltipTrigger>
-          </Tooltip>
+          </Tooltip> */}
           <Tooltip>
             <TooltipTrigger>
-              <Button
-                variant="outline"
-                className="rounded-full size-14 cursor-pointer"
+              <a
+                href={`https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={!lat || !lng ? "pointer-events-none" : ""}
+                aria-disabled={!lat || !lng}
               >
-                <Car className="size-5" />
-              </Button>
+                <Button
+                  variant="outline"
+                  className="rounded-full size-14 cursor-pointer"
+                  disabled={!lat || !lng}
+                >
+                  <Car className="size-5" />
+                </Button>
+              </a>
             </TooltipTrigger>
-            <TooltipContent>Directions</TooltipContent>
+            <TooltipContent>
+              {!lat || !lng ? "No Location" : "Get Directions"}
+            </TooltipContent>
           </Tooltip>
         </div>
       </div>
