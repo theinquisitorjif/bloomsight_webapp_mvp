febeb7ba8ecf7abcdd3595a1cc1ec7f9ebbb6f86
sophiadadla <dadlas@ufl.edu>
2025-08-18 22:58:04 -0400

    url

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -11,7 +11,7 @@ export const Footer = () => {
       <div className="mx-auto w-full max-w-[1000px] p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="https://flowbite.com/" className="flex items-center">
+            <a className="flex items-center">
               <img
                 src="/logo-only-nobg.png"
                 className="h-8 me-3"
