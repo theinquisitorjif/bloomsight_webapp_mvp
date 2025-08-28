33fdf00f04b0f5678ed595dea110577d755a75d8
sophiadadla <dadlas@ufl.edu>
2025-08-28 16:29:46 -0400

    config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/bloomsight_webapp_mvp/',
   server: {
     proxy: {
       '/algae-data': {
